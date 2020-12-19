43ada5cc71efae0f77af705b7e7e9399369d1a63
Atikur Rabbi <msawon94@gmail.com>
2020-12-19 13:40:59 +0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const PORT = 8080;
+const express = require("express");
+const app = express();
+app.use(express.json());
+app.get("/", function (req, res) {
+  //when we get an http get request to the root/homepage
+  res.send("Hello express templete");
+});
+
+app.listen(PORT, function () {
+  console.log(`Listening on Port ${PORT}`);
+});
